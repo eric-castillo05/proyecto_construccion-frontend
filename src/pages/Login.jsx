086bf06bbf64fe735eaf69bebb442eb8af72d8a2
eric-castillo05 <ericcastillo005@gmail.com>
2025-05-14 10:19:43 -0600

    chore: fix tailwind styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 
-function Login() {
+export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -43,43 +43,75 @@ function Login() {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <form onSubmit={handleLogin} className="bg-white p-8 rounded-xl border-2 border-green-500 shadow-lg w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
-
-                <div className="mb-4">
-                    <label className="block text-sm font-medium text-gray-700">Correo Electrónico</label>
-                    <input
-                        type="email"
-                        required
-                        className="w-full mt-1 px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-green-300"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        placeholder="correo@ejemplo.com"
+        <>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img
+                        alt="Logo"
+                        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                        className="mx-auto h-10 w-auto"
                     />
+                    <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
+                        Iniciar sesión en tu cuenta
+                    </h2>
                 </div>
 
-                <div className="mb-6">
-                    <label className="block text-sm font-medium text-gray-700">Contraseña</label>
-                    <input
-                        type="password"
-                        required
-                        className="w-full mt-1 px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-green-300"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        placeholder="••••••••"
-                    />
-                </div>
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form onSubmit={handleLogin} className="space-y-6">
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-900">
+                                Correo electrónico
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                                    placeholder="correo@ejemplo.com"
+                                />
+                            </div>
+                        </div>
 
-                <button
-                    type="submit"
-                    className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition"
-                >
-                    Iniciar Sesión
-                </button>
-            </form>
-        </div>
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium text-gray-900">
+                                    Contraseña
+                                </label>
+
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                                    placeholder="••••••••"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Iniciar sesión
+                            </button>
+                        </div>
+                    </form>
+
+
+                </div>
+            </div>
+        </>
     );
 }
-
-export default Login;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcssPostcss from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcssPostcss,
-        autoprefixer,
-      ],
-    },
-  },
+  plugins: [react(),tailwindcss()],
 })
